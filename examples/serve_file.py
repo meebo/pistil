65637b7eaf36ca8cd558dac18a6b772c11376c81
benoitc <benoitc@e-engura.org>
2011-08-04 21:10:15 +0200

    improve file serving example

--- examples/serve_file.py
+++ examples/serve_file.py
@@ -15,6 +15,14 @@ from pistil.tcp.gevent_worker import TcpGeventWorker
 
 CURDIR = os.path.dirname(__file__)
 
+try:
+    # Python 3.3 has os.sendfile().
+    from os import sendfile
+except ImportError:
+    try:
+        from _sendfile import sendfile
+    except ImportError:
+        sendfile = None
 
 def write_error(sock, status_int, reason, mesg):
     html = textwrap.dedent("""\
@@ -55,17 +63,70 @@ class HttpWorker(TcpGeventWorker):
             path = path[1:]
         
         real_path = os.path.join(CURDIR, "static", path)
-        if not os.path.exists(real_path):
+
+        if os.path.isdir(real_path):
+            lines = ["<ul>"]
+            for d in os.listdir(real_path):
+                fpath = os.path.join(real_path, d)
+                lines.append("<li><a href=" + d + ">" + d + "</a>")
+
+            data = "".join(lines)
+            resp = "".join(["HTTP/1.1 200 OK\r\n", 
+                            "Content-Type: text/html\r\n",
+                            "Content-Length:" + str(len(data)) + "\r\n",
+                            "Connection: close\r\n\r\n",
+                            data])
+            sock.sendall(resp)
+
+        elif not os.path.exists(real_path):
             util.write_error(sock, 404, "Not found", real_path + " not found")
         else:
-            ctype = mimetypes.guess_type(real_path)
+            ctype = mimetypes.guess_type(real_path)[0]
 
-            with open(real_path, 'r') as f:
-                resp = "".join(["HTTP/1.1 200 OK\r\n", 
-                                "Content-Type: " + ctype[0] + "\r\n",
+            if ctype.startswith('text') or 'html' in ctype:
+
+                try:
+                    f = open(real_path, 'rb')
+                    data = f.read()
+                    resp = "".join(["HTTP/1.1 200 OK\r\n", 
+                                "Content-Type: " + ctype + "\r\n",
+                                "Content-Length:" + str(len(data)) + "\r\n",
                                 "Connection: close\r\n\r\n",
-                                f.read()])
-                sock.sendall(resp)
+                                data])
+                    sock.sendall(resp)
+                finally:
+                    f.close()
+            else:
+
+                try:
+                    f = open(real_path, 'r')
+                    clen = int(os.fstat(f.fileno())[6])
+                    
+                    # send headers
+                    sock.send("".join(["HTTP/1.1 200 OK\r\n", 
+                                "Content-Type: " + ctype + "\r\n",
+                                "Content-Length:" + str(clen) + "\r\n",
+                                 "Connection: close\r\n\r\n"]))
+
+                    if not sendfile:
+                        while True:
+                            data = f.read(4096)
+                            if not data:
+                                break
+                            sock.send(data)
+                    else:
+                        fileno = f.fileno()
+                        sockno = sock.fileno()
+                        sent = 0
+                        offset = 0
+                        nbytes = clen
+                        sent += sendfile(sockno, fileno, offset+sent, nbytes-sent)
+                        while sent != nbytes:
+                            sent += sendfile(sock.fileno(), fileno, offset+sent, nbytes-sent)
+
+
+                finally:
+                    f.close()
 
 
 def main():
